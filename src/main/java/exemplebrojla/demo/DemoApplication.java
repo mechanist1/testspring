35fbf43b7634c10a0f185a05e2d6398bfb991373
mechanist1 <mlaouhiilyess@gmail.com>
2023-06-12 11:22:03 +0100

    first test for intellj using rest apis

---
+++ src/main/java/exemplebrojla/demo/DemoApplication.java
@@ -0,0 +1,15 @@
+package exemplebrojla.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+		System.out.print("hello world \n");
+		System.out.println("second test xD");
+	}
+
+}
---
+++ src/main/java/exemplebrojla/demo/student/studentcontroller.java
@@ -0,0 +1,4 @@
+package exemplebrojla.demo.student;
+
+public class studentcontroller {
+}
