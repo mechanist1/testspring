45b67125caec7f75c5e84c74b3c5edf4ebd82674
mechanist1 <mlaouhiilyess@gmail.com>
2023-06-12 11:33:48 +0100

    created the first rest api with a get method

--- src/main/java/exemplebrojla/demo/student/studentcontroller.java
+++ src/main/java/exemplebrojla/demo/student/studentcontroller.java
@@ -1,4 +1,21 @@
 package exemplebrojla.demo.student;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/test")
 public class studentcontroller {
+
+    @GetMapping
+    public List<String> findstudents(){
+        return( List.of(
+                "Ahmed",
+                "Ilyess"
+
+        ));
+    }
 }
