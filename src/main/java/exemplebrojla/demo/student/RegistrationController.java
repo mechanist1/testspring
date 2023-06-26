b6a015b0f54f8d7b6a9683f9102ffa14c04c3d9e
mechanist1 <mlaouhiilyess@gmail.com>
2023-06-26 13:20:39 +0100

    did the entity thing and instaled jpa and hibernate and did all the methods

---
+++ src/main/java/exemplebrojla/demo/student/RegistrationController.java
@@ -0,0 +1,30 @@
+package exemplebrojla.demo.student;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping
+public class RegistrationController {
+    private final RegistrationRepository registrationRepository;
+
+    @Autowired
+    public RegistrationController(RegistrationRepository registrationRepository) {
+        this.registrationRepository = registrationRepository;
+    }
+
+
+    @PostMapping
+    public void register(@RequestBody Registration r){
+        registrationRepository.save(r);
+
+    }
+    @DeleteMapping("/{id}")
+    public void deleteRegistration(@PathVariable Long id) {
+        registrationRepository.deleteById(id);
+    }
+@GetMapping("/{id}")
+    public Registration get(@PathVariable Long id ){
+        return registrationRepository.findById(id).orElse(null);
+}
+
+}
---
+++ src/main/java/exemplebrojla/demo/student/RegistrationRepository.java
@@ -0,0 +1,7 @@
+package exemplebrojla.demo.student;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RegistrationRepository extends JpaRepository<Registration,Long> {
+
+}
