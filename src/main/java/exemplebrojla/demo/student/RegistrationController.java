d8f8ce2c71d819890c585a13da5cecad73e3540e
mechanist1 <mlaouhiilyess@gmail.com>
2023-07-03 10:02:10 +0100

    the connection between the frontend and the backend is working

--- src/main/java/exemplebrojla/demo/student/RegistrationController.java
+++ src/main/java/exemplebrojla/demo/student/RegistrationController.java
@@ -1,9 +1,12 @@
 package exemplebrojla.demo.student;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/Registration")
+@CrossOrigin(origins = "http://localhost:4200")
 public class RegistrationController {
     private final RegistrationRepository registrationRepository;
 
@@ -14,8 +17,12 @@ public class RegistrationController {
 
 
     @PostMapping
-    public void register(@RequestBody Registration r){
+    public ResponseEntity<String> register(@RequestBody Registration r){
         registrationRepository.save(r);
+        System.out.println("This is the request body: " + r);
+        String res="its working";
+        return ResponseEntity.status(HttpStatus.OK).body(res);
+
 
     }
     @DeleteMapping("/{id}")
