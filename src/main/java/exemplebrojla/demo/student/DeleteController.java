4198bc03cc01aff8dde7cfc7e2f7d66279ad711e
mechanist1 <mlaouhiilyess@gmail.com>
2023-07-05 10:59:57 +0100

    all the CRUD works correctly expept for the put request which is easy but will take much time

--- src/main/java/exemplebrojla/demo/student/DeleteController.java
+++ src/main/java/exemplebrojla/demo/student/DeleteController.java
@@ -1,27 +1,27 @@
-package exemplebrojla.demo.student;
+    package exemplebrojla.demo.student;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+    import org.springframework.beans.factory.annotation.Autowired;
+    import org.springframework.http.HttpStatus;
+    import org.springframework.http.ResponseEntity;
+    import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+    import java.util.List;
 
-@RestController
-@RequestMapping("/Users")
-@CrossOrigin(origins = "http://localhost:4200")
-public class DeleteController {
+    @RestController
+    @RequestMapping("/Users")
+    @CrossOrigin(origins = "http://localhost:4200")
+    public class DeleteController {
 
-    private final RegistrationRepository registrationRepository;
+        private final RegistrationRepository registrationRepository;
 
-    @Autowired
-    public DeleteController(RegistrationRepository registrationRepository) {
-        this.registrationRepository = registrationRepository;
+        @Autowired
+        public DeleteController(RegistrationRepository registrationRepository) {
+            this.registrationRepository = registrationRepository;
+        }
+
+        @DeleteMapping("/{id}")
+        public void deleteUser (@PathVariable long id){
+            System.out.println("deleted the id="+id);
+            registrationRepository.deleteById(id);
+        }
     }
-
-    @DeleteMapping("/{id}")
-    public void deleteUser (@PathVariable long id){
-
-        registrationRepository.deleteById(id);
-    }
-}
