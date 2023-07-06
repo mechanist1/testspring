f1356704a79c1de90b138e7aedbdf9726dab2166
mechanist1 <mlaouhiilyess@gmail.com>
2023-07-06 10:16:25 +0100

    added a new entity and created a new table for users for the signup page

---
+++ src/main/java/exemplebrojla/demo/student/AddController.java
@@ -0,0 +1,32 @@
+package exemplebrojla.demo.student;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/signup")
+public class AddController {
+
+    private UsersRepository usersRepository;
+
+    @Autowired
+    public AddController(UsersRepository usersRepository) {
+        this.usersRepository = usersRepository;
+    }
+
+    @PostMapping
+    public ResponseEntity<String> add(@RequestBody User res){
+        if(res.getPassword()!= res.getPasswordagain()){
+           return ResponseEntity.status(HttpStatus.OK).body("The passwords don't match");
+        }
+        usersRepository.save(res);
+        return ResponseEntity.status(HttpStatus.OK).body("User saved");
+    }
+
+
+}
