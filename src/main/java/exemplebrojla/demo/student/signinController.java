707f97100222edda27bd1b353c5a8d3e7f373a86
mechanist1 <mlaouhiilyess@gmail.com>
2023-07-07 11:22:12 +0100

    final minus the jwt

--- src/main/java/exemplebrojla/demo/student/signinController.java
+++ src/main/java/exemplebrojla/demo/student/signinController.java
@@ -1,12 +1,13 @@
 package exemplebrojla.demo.student;
 
-import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/signin")
@@ -24,11 +25,11 @@ public class signinController {
         Optional<User> existingUser = usersRepository.findByEmail(user.getEmail());
 
         if (existingUser.isPresent() && existingUser.get().getpassword().equals(user.getpassword())) {
-            // User with the provided email and password exists
+
             return ResponseEntity.ok("user exists welcome");
         } else {
 
-            // User doesn't exist or the password doesn't match
+
             return ResponseEntity.ok("doesn't exist");
         }
     }
